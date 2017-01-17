c26ad946a15c69895b3a317c1323a14eabf6936a
AjinkyaPatil1121 <ajinkya.patil1121@gmail.com>
2017-01-16 17:36:17 -0800

    added delete feature,refactored some segments,other improvements

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $(".titleWidgets a").click(function() {	
 
@@ -5,17 +6,9 @@ $(document).ready(function() {
     	document.getElementById("addTaskForm").reset();
 
  	  	// change visibility of div Task pane and overlay
-    	var elPane = document.getElementsByClassName("overlayPane");
-    	elPane[0].style.visibility = "visible";
-
-    	var eltask = document.getElementsByClassName("addTaskPane");
-    	eltask[0].style.visibility = "visible";
-
+ 	  	turnTaskOverlayPane("visible");
     });
-});
-
-$("#addTaskForm").submit(function(e){
-    return false; // for preventing default page reload on submit behavior
+    loadToDo();
 });
 
 function taskObject(title, description)
@@ -26,34 +19,111 @@ function taskObject(title, description)
 	this.status = "new";
 }
 
-function processForm()
+function turnTaskOverlayPane(status)
 {
-	// change overlay visibility
 	var elPane = document.getElementsByClassName("overlayPane");
-    elPane[0].style.visibility = "hidden";
-
-    var eltask = document.getElementsByClassName("addTaskPane");
-    eltask[0].style.visibility = "hidden";
+    elPane[0].style.visibility = status;
 	
-	// get form elements
-	var elTaskTitle = document.getElementById("taskTitle");
-	var elTaskDesc = document.getElementById("taskDesc");
-
-	// input validation and handling
-
-	// store the elements in the object
-	var task = new taskObject(elTaskTitle.value,elTaskDesc.value);
-
-	// put the elements in local storage
-	sessionStorage.setItem(task.title,task); // key = task title, value = task object
-
-	// append a task list node to the div taskListView ul element
-
-	var liNode = document.createElement("li");
-	//var textNode = document.createTextNode(task1.title);
-	var textNode = document.createTextNode(task.title);
-
-	liNode.appendChild(textNode);
-
-	document.getElementById("taskList").appendChild(liNode);
+    var eltask = document.getElementsByClassName("addTaskPane");
+    eltask[0].style.visibility = status;
 }
+
+function loadToDo()
+{
+	// display list if exists
+	if (sessionStorage.getItem("theTaskList"))
+	{
+		//document.getElementById("taskListView").innerHTML = null;
+		//console.log(sessionStorage.getItem("theTaskList"));
+		//console.log("not inside");
+		document.getElementById("taskList").innerHTML = sessionStorage.getItem("theTaskList");
+	}
+	//otherwise, prompt to add stuff
+	else
+	{
+		//console.log("inside");
+		//document.getElementById("taskListView").innerHTML = "Hello! Please add new tasks from the + above";
+	}
+}
+
+$("#addTaskForm").submit(function(e){
+    return false; // for preventing default page reload on submit behavior
+});
+
+// Clicking on overlay pane for exiting
+
+$(".overlayPane").click(function(){
+    turnTaskOverlayPane("hidden");
+    //e.style.visibility = "hidden"; ??-research
+}); // TODO: add same for escape key
+
+// On task (li) mouseover display remove, details, done anchors
+$('#taskList').on('mouseover','li',function(){
+	var $allAnchor = $(this).find('a');
+	$allAnchor.css('display','block');
+});
+
+$('#taskList').on('mouseout','li',function(){
+	var $allAnchor = $(this).find('a');
+	$allAnchor.css('display','none');
+});
+
+$('#taskList').on('click','.listItemDel',function(e){
+	e.preventDefault();
+	$(this).parent().remove();
+	// remove item from localStorage too
+	sessionStorage.setItem("theTaskList",document.getElementById("taskList").innerHTML);
+});
+
+
+
+function processForm(submitType)
+{
+	// change overlay visibility off
+	turnTaskOverlayPane("hidden");
+	
+	//console.log(submitType);
+	
+	if (submitType == "Cancel")
+	{
+		
+	}
+	else
+	{
+		// get form elements for new task entry
+		var elTaskTitle = document.getElementById("taskTitle");
+		var elTaskDesc = document.getElementById("taskDesc");
+
+		// input validation and handling
+		// for blank
+
+		// store the elements in the object
+		var taskObj = new taskObject(elTaskTitle.value,elTaskDesc.value,new Date(),"new");
+
+		// put the elements in local storage
+		// save individual element per task -> object
+		sessionStorage.setItem(taskObj.title,taskObj); // key = task title, value = task object
+
+		var listItem = document.createElement("li");
+		
+		listItem.innerHTML = '<span class="drag"> : </span>' + taskObj.title + ' <a class="listItemDetail" style="display: none;" href="#"> MORE </a><a class="listItemDone" style="display: none;" href="#"> DONE </a><a class="listItemDel" style="display: none;" href="#"> X </a>';
+		//var listItem = '<li><span class="drag"> : </span><input class="listItem" value=' + taskObj.title + '><a class="listItemDetail" style="display: none;" href="#"> MORE </a><a class="listItemDone" style="display: none;" href="#"> DONE </a><a class="listItemDel" style="display: none;" href="#"> X </a></li>';
+
+		document.getElementById("taskList").appendChild(listItem);
+
+		// save entire list with key theTaskList, overwriting for all new entries with same name
+		// saves iterating the storage on refresh
+		sessionStorage.setItem("theTaskList",document.getElementById("taskList").innerHTML);		
+
+		// append a task list node to the div taskListView ul element
+
+		//var liNode = document.createElement("li");
+		//var textNode = document.createTextNode(task1.title);
+		//var textNode = document.createTextNode(task.title);
+
+		//liNode.appendChild(textNode);
+
+		//document.getElementById("taskList").appendChild(liNode);
+	}
+}
+
